768390b8d0b603e1028703eadbf04dfa9dc55f15
Christopher-Fan <FanXiaoLurn@outlook.com>
2026-05-07 18:53:38 -0500

    created initial build, gitignore, and added the dataset

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import tensorflow_hub as hub
+
+def main():
+    print("Hello World")
+
+if __name__ == "__main__":
+    main()
