b6a51e3b39c63034783b381ab48bc1ef6dec03ae
Christopher-Fan <FanXiaoLurn@outlook.com>
2026-05-07 19:11:11 -0500

    split dataset into training and testing splits

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import tensorflow_hub as hub
+import random
 
 def main():
     #load the dataset into a df
@@ -22,5 +23,20 @@ def main():
     df['label_enc'] = df['label'].map({'ham':0, 'spam': 1})
     print(df.head())
 
+    #split data into training and testing splits
+    x_train, x_test, y_train, y_test = train_test_split(
+        df['text'],
+        df['label_enc'],
+        test_size=0.2,
+        random_state=random.randint(1,1000)
+        )
+    
+    #convert from pandas to numpy arrays for performance
+    x_train_np = x_train.to_numpy()
+    x_test_np = x_test.to_numpy()
+    y_train_np = y_train.to_numpy()
+    y_test_np = y_test.to_numpy()
+
+    
 if __name__ == "__main__":
     main()
