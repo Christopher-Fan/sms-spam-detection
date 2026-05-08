9df23f912f6def6506f508e96d9eeddf6ac034ef
Christopher-Fan <FanXiaoLurn@outlook.com>
2026-05-07 19:05:42 -0500

    cleaned and refactored dataset

--- main.py
+++ main.py
@@ -10,7 +10,17 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 import tensorflow_hub as hub
 
 def main():
-    print("Hello World")
+    #load the dataset into a df
+    df = pd.read_csv('spam.csv', encoding='latin-1')
+    print(df.head())
+
+    #clean the dataframe
+    #replace unused columns
+    #encoding labels to better reflect the data
+    df = df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1)
+    df = df.rename(columns={'v1': 'label', 'v2': 'text'})
+    df['label_enc'] = df['label'].map({'ham':0, 'spam': 1})
+    print(df.head())
 
 if __name__ == "__main__":
     main()
